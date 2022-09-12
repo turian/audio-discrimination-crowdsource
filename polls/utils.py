86a63a757e2f36e34c865786f7796bdb89941024
yar <ahmadyar228@gmail.com>
2022-09-12 11:38:58 +0500

    frontend & task flow functions

--- polls/utils.py
+++ polls/utils.py
@@ -3,3 +3,7 @@ import random
 def batch_selector():
     random.seed()
     return random.random() < 0.9
+
+def present_task_for_user(task):
+    """mock function"""
+    return "www.example.com", "AAB"
