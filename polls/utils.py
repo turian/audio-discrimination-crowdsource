4854e70b29c92ce4086c401b2e58033a0c258302
yar <ahmadyar228@gmail.com>
2022-09-12 15:30:37 +0500

    common authflow for use in auth flow and task flow page

--- polls/utils.py
+++ polls/utils.py
@@ -1,4 +1,5 @@
 import random
+from django.utils import timezone
 
 def batch_selector():
     random.seed()
@@ -7,3 +8,18 @@ def batch_selector():
 def present_task_for_user(task):
     """mock function"""
     return "www.example.com", "AAB"
+
+def check_user_work_permission(user):
+    print("checking user permission")
+    minutes_after_should_rest = 15
+    minutes_after_can_continue = 75
+    current_time = timezone.now()
+    if not user.first_task_of_this_session_performed_at:
+        return True, False, 0
+    time_diff = current_time - user.first_task_of_this_session_performed_at
+    time_diff_minutes = time_diff.total_seconds() / 60
+    should_rest = time_diff_minutes > minutes_after_should_rest and \
+                  time_diff_minutes < minutes_after_can_continue
+    can_continue = time_diff_minutes > minutes_after_can_continue
+    rest_time = round(minutes_after_can_continue - time_diff_minutes) + 1
+    return can_continue, should_rest, rest_time
