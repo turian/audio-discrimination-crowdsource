fed7e7b257ee716f6eab9249e3ac8a2aae555a93
yar <ahmadyar228@gmail.com>
2022-09-22 20:57:00 +0500

    thank you front page and url

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("auth-flow/", views.AuthFlowView.as_view(), name="auth-flow"),
     path("task-flow/", views.TaskFlowView.as_view(), name="task-flow"),
     path("auth-token/", views.TokenView.as_view(), name="auth-token"),
-
+    path("thank-you/", views.ThanksView.as_view(), name="thank-you"),
     # APIs
     path("api/v1/admin-api/", views.AdminAPIView.as_view()),
 ]
