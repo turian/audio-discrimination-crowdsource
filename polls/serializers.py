c968475d32d629f5da0e1acb34d5b05cc9295be4
yar <ahmadyar228@gmail.com>
2022-09-26 15:31:35 +0500

    batch tasks serializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -12,3 +12,20 @@ class TaskSerializer(ModelSerializer):
     class Meta:
         model = Task
         exclude = ["batch"]
+
+
+class BatchTaskSerializer(ModelSerializer):
+    tasks = TaskSerializer(many=True, write_only=True)
+
+    class Meta:
+        model = Batch
+        fields = "__all__"
+
+    def create(self, validated_data):
+        tasks = validated_data.pop("tasks", [])
+        batch = Batch.objects.create(**validated_data)
+
+        for task_dict in tasks:
+            task_dict["batch"] = batch
+            Task.objects.create(**task_dict)
+        return batch
