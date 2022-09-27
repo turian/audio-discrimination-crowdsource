2cdd5d51fe3bfe229a93c59a7b816a91b57c7ae8
yar <ahmadyar228@gmail.com>
2022-09-27 12:37:31 +0500

    option to set the created batch as current_batch_gold or current_batch_eval

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,20 +1,22 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Annotation, Batch, Task
+from rest_framework import serializers
+from .models import Annotation, Batch, Task, CurrentBatchEval, CurrentBatchGold
 
 
-class AnnotationSerializer(ModelSerializer):
+class AnnotationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Annotation
         fields = "__all__"
 
 
-class TaskSerializer(ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         exclude = ["batch"]
 
 
-class BatchTaskSerializer(ModelSerializer):
+class BatchTaskSerializer(serializers.ModelSerializer):
+    set_to_current_batch_gold = serializers.BooleanField(default=False)
+    set_to_current_batch_eval = serializers.BooleanField(default=False)
     tasks = TaskSerializer(many=True, write_only=True)
 
     class Meta:
@@ -23,9 +25,20 @@ class BatchTaskSerializer(ModelSerializer):
 
     def create(self, validated_data):
         tasks = validated_data.pop("tasks", [])
+        set_to_current_batch_gold = validated_data.pop("set_to_current_batch_gold")
+        set_to_current_batch_eval = validated_data.pop("set_to_current_batch_eval")
         batch = Batch.objects.create(**validated_data)
 
         for task_dict in tasks:
             task_dict["batch"] = batch
             Task.objects.create(**task_dict)
+
+        # set to current_batch_gold or current_batch_eval
+        if set_to_current_batch_gold and batch.is_gold:
+            current_batch_gold = CurrentBatchGold(current_batch_gold=batch)
+            current_batch_gold.save()
+        elif set_to_current_batch_eval and not batch.is_gold:
+            current_batch_eval = CurrentBatchEval(current_batch_eval=batch)
+            current_batch_eval.save()
+
         return batch
