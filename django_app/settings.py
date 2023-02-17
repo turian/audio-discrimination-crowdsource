89e568771fc916e8155fa409dd17830cd5b60032
Joseph Turian <turian@gmail.com>
2023-02-17 22:17:34 +0100

    Bugfix

--- django_app/settings.py
+++ django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import os.path
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -18,6 +19,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+assert os.path.exists(BASE_DIR / ".env")
 load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +33,7 @@ CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS", "https://127.0.0.1 https://localhost"
 ).split(" ")
 
-DEBUG = os.getenv("DEBUG", "False") == True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
 
