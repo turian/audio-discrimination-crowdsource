78a3b2e85a6e56a3a7919cd0ab0ca5be84d02127
najamF18 <najamazam420@gmail.com>
2023-02-25 11:59:32 -0600

    fixed linter issues in polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
 
 
 class SingletonModel(models.Model):
