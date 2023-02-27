31b60a35d0d784e390626a6bd1a6899c2df923d5
najamF18 <najamazam420@gmail.com>
2023-02-27 11:54:56 -0600

    settings modified to read envs from .env

--- django_app/settings.py
+++ django_app/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+load_dotenv(".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
