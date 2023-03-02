71a70d067817d9510a8262f49dcde8a4ecdf10ea
Amusat Haki Adeyemi <adeyemihaki@gmail.com>
2023-03-02 18:00:35 +0100

    fixed typo

--- polls/templatetags/custom_tags.py
+++ polls/templatetags/custom_tags.py
@@ -1,10 +1,10 @@
 from django import template
 
-from .utils import get_user_num_task
+from ..utils import get_user_num_task
 
 register = template.Library()
 
 
-@register.filter
+@register.filter(name="get_user_num_task")
 def get_user_num_tasks(user):
     return get_user_num_task(user)
