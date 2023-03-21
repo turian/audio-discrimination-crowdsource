0e58bb8c7d39b1f40541c71035a53fd16bec5715
Amusat Haki Adeyemi <adeyemihaki@gmail.com>
2023-03-21 15:35:12 +0100

    fixed isort

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class SingletonModel(models.Model):
